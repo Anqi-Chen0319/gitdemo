cd6afa757b3287cbe6f3dafc72511cdf59f57b93
Anqi-Chen0319 <anqichen0319@berkeley.edu>
2019-10-03 11:58:26 -0700

    demo-script.R: adding demo script

--- code/demo-script.R
+++ code/demo-script.R
@@ -7,11 +7,58 @@
 # Author(s): Anqi Chen
 # Date: 10-02-2019
 
+
 # Packages
 library("dplyr")
 library("ggplot2")
+library("readr")
+
 
 # Importing a data
 dat <- read.csv("../data/nba2018-players.csv",sep=",")
 was <- filter(dat,team=="WAS")
+write.csv(was,"../data/was.csv")
 
+
+# Exportin some R output
+
+# exporting structure of dat
+str(dat)
+sink("../output/data-structure.txt")
+str(dat)
+sink()
+
+# exporting summary for WAS
+sink("../output/summary-warriors.txt")
+summary(was)
+sink()
+
+# exporting summmary about LAL
+lakers <- filter(dat,team=="LAL")
+sink("../output/summary-lakers.txt")
+summary(lakers)
+sink()
+
+
+# Exporting graphs
+
+# frequency of age
+jpeg("../images/histogram-age.jpeg",width = 600,height = 400)
+hist(dat$age,xlab = "Age",ylab = "Count")
+dev.off()
+
+# png for scatterplot
+png(filename = "../images/scatterplot-height-weight.png")
+plot(dat$height, dat$weight, pch = 20, 
+     xlab = 'Height', ylab = 'Height')
+dev.off()
+
+# second scatterplot for height and weight
+png("../images/scatterplot2-height-weight.png")
+ggplot(dat,aes(x=dat$height,y=dat$weight))+
+  geom_point(aes(size=2))+
+  labs(x="Heigh",y="Weight")+
+  theme_bw()
+dev.off()
+
+#
